b764270789ee811c212c3be4c20038ae2fc8a487
brad.boggs <brad.boggs@aggrowth.com>
2025-06-28 01:46:37 -0500

    fix typos

--- validator.py
+++ validator.py
@@ -24,11 +24,13 @@ if __name__ == "__main__":
         )
         cosine_similarites = util.pairwise_cos_sim(stem_embeddings, anchor_embeddings)
 
-        data_with_cos_sin = data.with_columns(
+        data_with_cos_sim = data.with_columns(
             pl.Series(name="cosine_similarity", values=cosine_similarites.cpu().numpy())
         )
 
-        data_with_cos_sin.with_columns(
+        data_with_cos_sim.with_columns(
             (pl.col("cosine_similarity") < THRESHOLD).cast(pl.Int8).alias("drift_flag"),
-            (pl.col("stem_text").str.split(" ").list.len() > 15 ).cast(pl.Int8).alias("lenght_flag")
+            (pl.col("stem_text").str.split(" ").list.len() > 15)
+            .cast(pl.Int8)
+            .alias("length_flag"),
         ).to_pandas().to_csv(f"ai_stems_with_sims-{model}.csv")
